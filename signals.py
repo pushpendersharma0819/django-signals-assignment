9702dbc7039ffd61e600b182507e8aaf5c0569d7
Pushpender sharma <pushpendersharma4889@gmail.com>
2026-06-10 12:34:47 +0530

    Add files via upload

--- signals.py
+++ signals.py
@@ -2,6 +2,7 @@ import time
 import threading
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.db import connection
 from .models import Student
 
 @receiver(post_save, sender=Student)
@@ -14,3 +15,9 @@ def student_saved(sender, instance, **kwargs):
 
     #question 2
     print("signal Thread ID:", threading.get_ident())
+
+    #question 3
+    print("Inside Signal")
+    print("In Atomic Block:", connection.in_atomic_block)
+
+    raise Exception("Signal Exception for Transection Test")
